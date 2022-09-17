d51b85b15f665ceb78f47bff393422ffc9b05e0d
Xin Zhang <amanda4zhangxin@gmail.com>
2022-09-17 21:50:55 +0800

    Removed the Jekyll project; created my own contents

---
+++ dist/js/main.js
@@ -0,0 +1,17 @@
+// const menuBtn = document.querySelector('.menu-btn');
+// const burger = document.querySelector('.menu-btn__burger');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+// let menuShown = false;
+// menuBtn.addEventListener('click', toggleMenu);
+
+// function toggleMenu() {
+//     if(!menuShown) {
+//         burger.classList.add('open');
+//         menuShown = true;
+//     } else {
+//         burger.classList.remove('open');
+//         menuShown = false;
+//     }
+// }
+
