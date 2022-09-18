9272a9d4e8d92273f8840ff69164c6528504568d
Xin Zhang <amanda4zhangxin@gmail.com>
2022-09-18 10:19:04 +0800

    Added support for display on mobile in portrait orientation

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-// const menuBtn = document.querySelector('.menu-btn');
-// const burger = document.querySelector('.menu-btn__burger');
-const navItems = document.querySelectorAll('.menu-nav__item');
+const menuBtn = document.querySelector('.menu-btn');
+const burger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('nav');
+const menuNav = document.querySelector('menu-nav');
+// const navItems = document.querySelectorAll('.menu-nav__item');
 
-// let menuShown = false;
-// menuBtn.addEventListener('click', toggleMenu);
+let menuShown = false;
+menuBtn.addEventListener('click', toggleMenu);
 
-// function toggleMenu() {
-//     if(!menuShown) {
-//         burger.classList.add('open');
-//         menuShown = true;
-//     } else {
-//         burger.classList.remove('open');
-//         menuShown = false;
-//     }
-// }
+function toggleMenu() {
+    if(!menuShown) {
+        burger.classList.add('open');
+        nav.classList.add('open');
+        // menuNav.classList.add('open');
+        menuShown = true;
+    } else {
+        burger.classList.remove('open');
+        nav.classList.remove('open');
+        // menuNav.classList.remove('open');
+        menuShown = false;
+    }
+}
 
